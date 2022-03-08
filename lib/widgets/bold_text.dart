44db7620dbd390d9e02c271a07326da41bea462d
Yannick Mauray <yannick.mauray@arcantel.ch>
2022-03-08 16:31:13 +0100

    Added alignment for BoldText

--- lib/widgets/bold_text.dart
+++ lib/widgets/bold_text.dart
@@ -2,12 +2,19 @@ import 'package:flutter/material.dart';
 
 class BoldText extends StatelessWidget {
   final String data;
-  const BoldText(this.data, {Key? key}) : super(key: key);
+  final TextAlign? textAlign;
+
+  const BoldText(
+    this.data, {
+    this.textAlign,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       data,
+      textAlign: textAlign,
       style: const TextStyle(
         fontWeight: FontWeight.bold,
       ),
