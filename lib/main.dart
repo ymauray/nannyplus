eaac80d940473ec38e3a51b03340a68f7ac60076
Yannick Mauray <yannick.mauray@gmail.com>
2022-10-29 13:03:23 +0200

    dev: removed old code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
@@ -8,7 +6,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl_standalone.dart';
 import 'package:nannyplus/app.dart';
 import 'package:nannyplus/utils/notification_util.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
@@ -28,14 +25,6 @@ Future<void> main() async {
   });
 
   await findSystemLocale();
-
-  final tempDir = await getTemporaryDirectory();
-  final tempFiles = tempDir.listSync();
-  for (final file in tempFiles) {
-    if (file is File) {
-      file.deleteSync();
-    }
-  }
 
   runApp(
     const NannyPlusApp(),
