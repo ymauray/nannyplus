531bfb252099e2cb0543f197fad350758694411c
Yannick Mauray <yannick.mauray@arcantel.ch>
2022-12-13 15:33:22 +0100

    feat: removed local_notification as a dependency

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,14 @@ import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl_standalone.dart';
 import 'package:nannyplus/app.dart';
-import 'package:nannyplus/utils/notification_util.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   GoogleFonts.config.allowRuntimeFetching = false;
-  await NotificationUtil().init();
-  await NotificationUtil().requestIOSPermissions();
+  // await NotificationUtil().init();
+  // await NotificationUtil().requestIOSPermissions();
   tz.initializeTimeZones();
   final currentTimeZone = await FlutterNativeTimezone.getLocalTimezone();
   tz.setLocalLocation(tz.getLocation(currentTimeZone));
