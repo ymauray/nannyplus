d792d5502ba2ec1fc2eb9606ad79c757d7ec529e
Yannick Mauray <yannick.mauray@gmail.com>
2023-03-06 13:25:35 +0100

    fix: added document icon on the child form.

--- lib/src/child_form/child_form.dart
+++ lib/src/child_form/child_form.dart
@@ -4,6 +4,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gettext_i18n/gettext_i18n.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
@@ -510,6 +511,22 @@ class _DocumentList extends StatelessWidget {
                       ),
                     ),
                   ),
+                  if (file.bytes.isNotEmpty)
+                    const IconButton(
+                      onPressed: null,
+                      icon: Icon(
+                        FontAwesomeIcons.database,
+                        color: Colors.green,
+                      ),
+                    ),
+                  if (!file.bytes.isNotEmpty)
+                    const IconButton(
+                      onPressed: null,
+                      icon: Icon(
+                        FontAwesomeIcons.fileCircleExclamation,
+                        color: Colors.red,
+                      ),
+                    ),
                   IconButton(
                     onPressed: () async {
                       final newLabel =
